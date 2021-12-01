d097c17928cb05e72d0040ca33979561a449d384
Michal Gagala <michal@gagala.dev>
2021-12-01 16:58:41 +0000

    removed sslc notes xD

--- D1/src/Lib.hs
+++ D1/src/Lib.hs
@@ -9,7 +9,3 @@ count (x : y : xs) s
     | y > x = f $ s + 1
     | otherwise = f s
     where f = count (y : xs)
-
--- point of seminars isn't to give solutions regarding answers to questions
--- its to have discussions
-
